214eabee87c740cc2b2d1cf4a5ba0f5f90dfa4fc
Emil <emilsabalvoro@gmail.com>
2024-09-10 14:56:49 +0800

    implemented laravel notifications for email

---
+++ app/Notifications/WelcomeEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use Models\VoucherCode;
+
+class WelcomeEmail extends Notification
+{
+    use Queueable;
+
+    private $voucherCode;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(VoucherCode $voucherCode)
+    {
+        $this->voucherCode = $voucherCode;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('Here is your voucher!')
+                    ->line('Your voucher code is: ')
+                    ->line('$this->voucherCode->code');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
