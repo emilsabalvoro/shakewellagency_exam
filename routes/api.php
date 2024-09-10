0cd966e0571b092deadc8f8f33fa80d941661150
Emil <emilsabalvoro@gmail.com>
2024-09-10 15:25:22 +0800

    moved routes from web.php to api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('voucher/generate', [VoucherCodeController::class, 'generate']);
+    Route::get('voucher', [VoucherCodeController::class, 'index']);
+    Route::delete('voucher/{id}', [VoucherCodeController::class, 'destroy']);
+});
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,3 @@ use App\Http\Controllers\Api\VoucherCodeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('register', [AuthController::class, 'register']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('voucher/generate', [VoucherCodeController::class, 'generate']);
-    Route::get('voucher', [VoucherCodeController::class, 'index']);
-    Route::delete('voucher/{id}', [VoucherCodeController::class, 'destroy']);
-});
